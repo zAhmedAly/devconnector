103d12093a1f98fd19a2c027b94e1efbff07103b
Ahmed Aly <aaoflaca@gmail.com>
2020-07-27 13:54:34 -0700

    Adjust mongoDB options for useFindAndModify

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
-      useFindAndModify: true,
+      useFindAndModify: false,
     });
     console.log('===> Connected to MongoDB Server ...');
   } catch (err) {
